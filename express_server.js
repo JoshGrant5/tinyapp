f1800f17397bc480a471378cc67a2065d5b77d44
JoshGrant5 <joshgg@icloud.com>
2020-09-28 21:29:25 +0000

    Added some comments for personal reference

--- express_server.js
+++ express_server.js
@@ -7,18 +7,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// get me a route to page "/" => if we get a response, send the page "Hello!"
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// is the port open? Once it is, log the message
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// get me a route to page "/urls.json" => if response, send it the json string of urlDatabase
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// get me a route to page "/hello" => if response, send it the json string of this html code
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+}); // curl -i http://localhost:8080/hello will give us back this string, while redirecting to "/hello" will show us Hello World => converted from json to html
+
