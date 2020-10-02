8bbf898b0797f5068413e74d394c9bbbd8988df9
JoshGrant5 <joshgg@icloud.com>
2020-10-02 05:13:58 +0000

    Removed cookie-parser as dependancy

--- express_server.js
+++ express_server.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-const cookieParser = require('cookie-parser');
 const request = require('request');
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session');
 const { generateRandomString, emailExists, urlsForUser, visitorCount } = require('./helpers');
 
 // middleware
-app.use(cookieParser())
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
